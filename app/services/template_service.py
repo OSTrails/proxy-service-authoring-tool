294b661b5e229cb5e2a895b3089b90a1bf3a4651
saracuriel <144734028+saracuriel@users.noreply.github.com>
2026-04-15 16:13:00 +0200

    Added missing ","

--- app/services/template_service.py
+++ app/services/template_service.py
@@ -10,7 +10,7 @@ TEMPLATE_DIR = os.path.join(
 env = Environment(
     loader=FileSystemLoader(TEMPLATE_DIR),
     autoescape=False,
-    extensions=["jinja2.ext.do"]  # REQUIRED for {% do %},
+    extensions=["jinja2.ext.do"] , # REQUIRED for {% do %}
     auto_reload=True # Avoid falling into caching templates
 )
 
